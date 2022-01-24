4bf79b2cfabe45db7b927b959384e56e93968a97
Michael Mandel <michael@mandel.dev>
2022-01-24 18:26:11 +0100

    ops: adjust description

--- Formula/beam.rb
+++ Formula/beam.rb
@@ -1,5 +1,5 @@
 class Beam < Formula
-  desc "a fuzzy finder interface on top of the Teleport CLI"
+  desc "beam is a fuzzy finder interface on top of the Teleport CLI"
   homepage "https://github.com/MichaelMandel26/beam"
   url "https://github.com/MichaelMandel26/beam/archive/v0.2.9.tar.gz"
   sha256 "cb417b8bcfa5c7a83aebde412eb121b4a4a289b583cd009a753381e4c6623114"
@@ -8,7 +8,6 @@ class Beam < Formula
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", *std_cargo_args
   end
 
