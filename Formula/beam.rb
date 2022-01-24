6f71b8408653e13d3b30b51efb41ae268600ff66
Michael Mandel <michael@mandel.dev>
2022-01-24 18:30:02 +0100

    ops: adjust description

--- Formula/beam.rb
+++ Formula/beam.rb
@@ -1,5 +1,5 @@
 class Beam < Formula
-  desc "Enhance your teleport workflow with a fuzzy finder interface on top of the Teleport CLI"
+  desc "Enhance your teleport workflow with a fuzzy finder interface"
   homepage "https://github.com/MichaelMandel26/beam"
   url "https://github.com/MichaelMandel26/beam/archive/v0.2.9.tar.gz"
   sha256 "cb417b8bcfa5c7a83aebde412eb121b4a4a289b583cd009a753381e4c6623114"
