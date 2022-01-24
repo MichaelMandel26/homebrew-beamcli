3ccc04525342b66c9d3319d187bd0685f59ae96b
Michael Mandel <michael@mandel.dev>
2022-01-24 19:18:24 +0100

    ops: set tag to v0.2.10 & add teleport dependency

--- Formula/beam.rb
+++ Formula/beam.rb
@@ -1,12 +1,13 @@
 class Beam < Formula
   desc "Enhance your teleport workflow with a fuzzy finder interface"
   homepage "https://github.com/MichaelMandel26/beam"
-  url "https://github.com/MichaelMandel26/beam/archive/v0.2.9.tar.gz"
+  url "https://github.com/MichaelMandel26/beam/archive/v0.2.10.tar.gz"
   sha256 "cb417b8bcfa5c7a83aebde412eb121b4a4a289b583cd009a753381e4c6623114"
   license "MIT"
 
   depends_on "rust" => :build
-
+  depends_on "teleport"
+  
   def install
     system "cargo", "install", *std_cargo_args
   end
