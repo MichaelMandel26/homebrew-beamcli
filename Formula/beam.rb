4647a423b753c426cb7cb69199dc83f6f5dcdb34
Michael Mandel <30029605+MichaelMandel26@users.noreply.github.com>
2022-01-30 22:30:39 +0000

    beam: update 0.2.10 bottle.

--- Formula/beam.rb
+++ Formula/beam.rb
@@ -7,8 +7,9 @@ class Beam < Formula
 
   bottle do
     root_url "https://github.com/MichaelMandel26/homebrew-beamcli/releases/download/beam-0.2.10"
-    sha256 cellar: :any_skip_relocation, big_sur:      "5556ec3ee038d03b50793f29d38b4c9be66cdca1eb08bef03cb7160383df5396"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a404e14bc06a6a8ba78e75e55e092f619bcb5a868839493d39f8019c2e039600"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur:      "daf9b492402a0bfb2928704383c60e5ee9b936319c1c6816989f037e1c40633e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d985b0cfc5a7e8e28aa079b5b5f7445e1bc1fe1cdb8ea4cc92c80b5991168daa"
   end
 
   depends_on "rust" => :build
