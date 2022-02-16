f11dc744483c03418acc5ae55c7e48ffad613422
Michael Mandel <michael@mandel.dev>
2022-02-17 00:25:11 +0100

    monterey bottle (#5)
    * ops: add monterey bottle
    
    * style: fix formula style changes
    
    * ops: change rebuild

--- Formula/beam.rb
+++ Formula/beam.rb
@@ -8,8 +8,9 @@ class Beam < Formula
   bottle do
     root_url "https://github.com/MichaelMandel26/homebrew-beamcli/releases/download/beam-0.2.10"
     rebuild 2
-    sha256 cellar: :any_skip_relocation, big_sur:      "cfeb6804fdbac6d6b6a6a8427d2ca1a82d27ab56413e0775618f642ae3df3673"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "66b64864417cfb4ca4d97b00989a2a68c99aa8e0777fa054cded61b4a0b5afd3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "41d36e9f41ce51c2a23ee515ac436488882eea3c7ce59b50fb88f1fdff8821e4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cfeb6804fdbac6d6b6a6a8427d2ca1a82d27ab56413e0775618f642ae3df3673"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "66b64864417cfb4ca4d97b00989a2a68c99aa8e0777fa054cded61b4a0b5afd3"
   end
 
   depends_on "rust" => :build
