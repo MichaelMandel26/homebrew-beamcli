0ed52a19ac9b3cf3564e1c2437e7518c2aeea158
Michael Mandel <michael.mandel@awin.com>
2022-05-25 16:14:54 +0200

    ops: add monterey bottle for v0.4.5

--- Formula/beam.rb
+++ Formula/beam.rb
@@ -7,8 +7,9 @@ class Beam < Formula
 
   bottle do
     root_url "https://github.com/MichaelMandel26/homebrew-beamcli/releases/download/beam-0.4.5"
-    sha256 cellar: :any_skip_relocation, big_sur:      "bee254414d35947ab2912315f848b6857a2b1a66568bd7d313f9e17a16fac291"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "8d8297f06b031ed4c9e32cdd34b18d03d0da2734c29d81d5e97854ca808b265d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee38eb17b178889d475f3bae968d5d57e122b10630788417e4cd74ac2d9a5f87"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bee254414d35947ab2912315f848b6857a2b1a66568bd7d313f9e17a16fac291"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d8297f06b031ed4c9e32cdd34b18d03d0da2734c29d81d5e97854ca808b265d"
   end
 
   depends_on "rust" => :build
