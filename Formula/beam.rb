d6d2f07c16411f2f5b2d0aab47a4c74cec89d293
Michael Mandel <michael.mandel@awin.com>
2022-08-02 13:11:22 +0200

    beam 0.4.6
    Closes #14.
    
    Signed-off-by: Michael Mandel <30029605+MichaelMandel26@users.noreply.github.com>

--- Formula/beam.rb
+++ Formula/beam.rb
@@ -1,8 +1,8 @@
 class Beam < Formula
   desc "Enhance your teleport workflow with a fuzzy finder interface"
   homepage "https://github.com/MichaelMandel26/beam"
-  url "https://github.com/MichaelMandel26/beam/archive/refs/tags/v0.4.5.tar.gz"
-  sha256 "73870c5ee2d5bb2b9e33c296f70fc422afd91eff449be6bf544fcc3e7259864b"
+  url "https://github.com/MichaelMandel26/beam/archive/refs/tags/v0.4.6.tar.gz"
+  sha256 "aba5d88ed396d7b04e2f2f1c6bbe06dc7a672773747b7e55526fd8fc2720b485"
   license "MIT"
 
   bottle do
